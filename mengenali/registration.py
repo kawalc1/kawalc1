402f04bbddc32a2b8846e05aaac11a17dc25a193
Sjappie Barhoofd <sam.theisens@gmail.com>
2014-08-30 15:56:25 +0200

    * using standard logging for debug info  * factored out duplicate logic for writing the transformed image

--- mengenali/registration.py
+++ mengenali/registration.py
@@ -4,58 +4,68 @@ import os
 import datetime
 import math
 import json
-
+import logging
 from os.path import join
 
+logging.basicConfig(level=logging.DEBUG)
 
-def create_response(image_file_name, success):
-    return json.dumps({'transformedUrl': image_file_name, 'success': success}, separators=(',', ':'))
+
+def print_result(result_writer, iteration, homography, transform, result):
+    row = [str(iteration), str(datetime.datetime.now()), homography, transform, result]
+    logging.info("output: " + str(row))
+    print row
+
+
+def create_response(image_path, success):
+    return json.dumps({'transformedUrl': image_path, 'success': success}, separators=(',', ':'))
+
+
+def write_transformed_image(image_transformed, homography, transform, good_enough_match, file_name, output_path,
+                            result_writer):
+    file_prefix = "~trans" if good_enough_match else "~bad"
+    transformed_image = file_prefix + "~hom" + str(homography) + "~warp" + str(transform) + "~" + file_name
+    image_path = join(output_path, transformed_image)
+    cv2.imwrite(image_path, image_transformed)
+
+    result = "good" if good_enough_match else "bad"
+    logging.info("%s image: %s", result, transformed_image)
+    # print_result(result_writer, 0, transformed_image, homography, transform, result)
+    return transformed_image
 
 
 def register_image(file_path, reference_form_path, output_path, result_writer):
     reference = cv2.imread(reference_form_path, 0)
-    print >> result_writer, "read reference"
+    logging.info("read reference %s", reference_form_path)
     orb = cv2.SIFT()
     kp2, des2 = orb.detectAndCompute(reference, None)
 
     image = cv2.imread(file_path, 0)
-    print >> result_writer, "read upload"
+    logging.info("read uploaded image %s", file_path)
     kp1, des1 = orb.detectAndCompute(image, None)
-    print >> result_writer, "detected orb"
+    logging.info("detected orb")
     bf = cv2.BFMatcher(cv2.NORM_L2)
     raw_matches = bf.knnMatch(des1, trainDescriptors=des2, k=2)
-    print >> result_writer, "knn matched"
+    logging.info("knn matched")
     matches = filter_matches(kp1, kp2, raw_matches)
     mkp1, mkp2 = zip(*matches)
     p1 = np.float32([kp.pt for kp in mkp1])
     p2 = np.float32([kp.pt for kp in mkp2])
-    print >> result_writer, "starting RANSAC"
+    logging.info("starting RANSAC")
     homography_transform, mask = cv2.findHomography(p1, p2, cv2.RANSAC, 5.0)
-    print >> result_writer, "RANSAC finished"
+    logging.info("RANSAC finished")
     homography, transform = check_homography(homography_transform)
 
     good_enough_match = check_match(homography, transform)
 
     h, w = reference.shape
     image_transformed = cv2.warpPerspective(image, homography_transform, (w, h))
-    print >> result_writer, "transformed image"
-    head, tail = os.path.split(file_path)
-    if good_enough_match:
-        # save the images
-        #transformed image
-        good_image_file_name = "~trans~hom" + str(homography) + "~warp" + str(transform) + "~" + tail
-        good_image = join(output_path, good_image_file_name)
-        cv2.imwrite(good_image, image_transformed)
-        print >> result_writer, "good image"
-        print_result(result_writer, 0, good_image, homography, transform, "good")
-        return create_response(good_image_file_name, True)
-    else:
-        bad_image_file_name = "~bad~hom" + str(homography) + "~warp" + str(transform) + "~" + tail
-        bad_image = join(output_path, bad_image_file_name)
-        cv2.imwrite(bad_image, image_transformed)
-        print >> result_writer, "bad image"
-        print_result(result_writer, 0, bad_image, homography, transform, "bad")
-        return create_response(bad_image_file_name, False)
+    logging.info("transformed image")
+    head, file_name = os.path.split(file_path)
+
+    transformed_image = write_transformed_image(image_transformed, homography, transform, good_enough_match, file_name,
+                                                output_path, result_writer)
+    logging.info("transformed %s", transformed_image)
+    return create_response(transformed_image, good_enough_match)
 
 
 def process_file(result_writer, count, root, file_name):
@@ -89,9 +99,9 @@ def check_homography(homography_transform):
     if homography > 0.01:
         # test=np.array([[10,20,20,10],[10,10,20,20],[1,1,1,1]])
         test = np.array([[10, 10, 1], [20, 10, 1], [20, 20, 1], [10, 20, 1]])
-        #do the check
+        # do the check
         trans = np.dot(test, homography_transform)
-        #print trans
+        # print trans
         dist1 = math.sqrt(math.pow(trans[0, 0] - trans[2, 0], 2) + math.pow(trans[0, 1] - trans[2, 1], 2))
         dist2 = math.sqrt(math.pow(trans[1, 0] - trans[3, 0], 2) + math.pow(trans[1, 1] - trans[3, 1], 2))
 
@@ -100,9 +110,3 @@ def check_homography(homography_transform):
         return homography, absolute_measure
     else:
         return homography, 0
-
-
-def print_result(result_writer, iteration, file_name, homography, transform, result):
-    row = [str(iteration), file_name, str(datetime.datetime.now()), homography, transform, result]
-    print >> result_writer, "output: " + str(row)
-    print row
