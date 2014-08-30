b24de05172eee09cc33afa56d4336c02454a2a35
Sjappie Barhoofd <sam.theisens@gmail.com>
2014-08-30 21:14:26 +0200

    * added unsuccessful form registration case  * added option to overwrite resources if outcome changed

--- test/registration_test.py
+++ test/registration_test.py
@@ -2,27 +2,51 @@ import unittest
 import registration
 import tempfile
 import json
+import io
 from os.path import join, exists
 
-class RegistrationTest(unittest.TestCase):
 
+class RegistrationTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        bla = True
+        cls.overwrite_resources = False
 
-    def test_registration_succeeds_for_reference_form(self):
-        image = 'test/resources/forms/original/1773007-005324400804.jpg'
+    def assert_registration_as_expected(self, c1_form, expected_result):
+        originals_path = 'test/resources/forms/original/'
+        image = join(originals_path, c1_form)
         reference_form = 'static/datasets/referenceform.jpg'
         expected_output_path = 'test/resources/forms/transformed'
         output_path = tempfile.gettempdir()
         registration_output = registration.register_image(image, reference_form, output_path, None)
         registration_json = json.loads(registration_output)
+        success = registration_json["success"]
 
-        temp_image = join(output_path, registration_json["transformedUrl"])
-        reference_image = join(expected_output_path, registration_json["transformedUrl"])
-        self.assertTrue(exists(temp_image))
-        with open(temp_image) as actual_image, open(reference_image) as expected_image:
-            self.assertEqual(actual_image.read(), expected_image.read())
+        self.assertEqual(success, expected_result, msg="registration success should be as expected")
+
+        image_url = registration_json["transformedUrl"]
+        temp_image_path = join(output_path, image_url)
+        expected_image_path = join(expected_output_path, image_url)
+        self.assertTrue(exists(temp_image_path))
+        with io.FileIO(temp_image_path, mode='rb') as actual_image, io.FileIO(expected_image_path,
+                                                                              mode='rb') as expected_image:
+            actual_bytes = actual_image.readall()
+            expected_bytes = expected_image.readall()
+
+        if actual_bytes != expected_bytes and self.overwrite_resources:
+            with io.FileIO(expected_image_path, mode='wb') as file_to_over_write:
+                file_to_over_write.write(actual_bytes)
+                file_to_over_write.flush()
+        self.assertEqual(actual_bytes, expected_bytes, msg="images should be identical")
+
+    def test_overwrite_resources_should_be_false(self):
+        self.assertFalse(self.overwrite_resources)
+
+    def test_registration_succeeds_for_reference_form(self):
+        self.assert_registration_as_expected('1773007-005324400804.jpg', True)
+
+    def test_registration_fails_for_incorrect_form(self):
+        self.assert_registration_as_expected('1386928-005381002001.jpg', False)
+
 
 if __name__ == '__main__':
     unittest.main()
