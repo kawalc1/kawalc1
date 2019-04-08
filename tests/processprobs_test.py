fc0d51bc3556a0727f495185f611c9254448094f
SamTheisens <sam.theisens@gmail.com>
2019-04-08 09:00:54 +0700

    Fixes test for #6 case

--- tests/processprobs_test.py
+++ tests/processprobs_test.py
@@ -1,20 +1,16 @@
 import unittest
 import json
 from mengenali import processprobs as pp
-from mengenali.processprobs import print_outcome
+from mengenali.processprobs import print_outcome, print_outcome_parsable
 
 
 class ProcessProbabilitiesTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-
-        json_file = open('./resources/probabilities/1773007-005324400804.json')
-        cls.json_data = json.load(json_file)
-
         config_file = open('../static/datasets/digit_config_pilpres_2019.json')
         cls.config = json.load(config_file)
-        json_file.close()
+        config_file.close()
 
     def test_json_is_read_correctly(self):
         numpy_table = pp.read_json(self.json_data)
@@ -34,56 +30,30 @@ class ProcessProbabilitiesTest(unittest.TestCase):
         json_data_yosua = json.load(json_file_yosua)
         json_file_yosua.close()
 
-        outcomes = pp.get_possible_outcomes_for_config(self.config, json_data_yosua["probabilities"], 11, print_outcome)
-        most_likely = outcomes[0][0]
-        print("most likely", str(most_likely))
-        self.assertEqual(most_likely['jokowi'], 186)
-        self.assertEqual(most_likely['prabowo'], 117)
-        self.assertEqual(most_likely['jumlah'], 303)
-        self.assertAlmostEqual(most_likely['confidence'], 0.7020011959926853)
+        outcomes = pp.get_possible_outcomes_for_config(self.config, json_data_yosua["probabilities"], 11, print_outcome_parsable)
+        presidential = outcomes[0][0]['numbers']
 
-    def test_get_numbers2(self):
-        json_file = open('./resources/probabilities/IMG_4221.json')
-        json_data = json.load(json_file)
+        self.assertEqual(presidential[0]['number'], 118)
+        self.assertEqual(presidential[0]['shortName'], 'jokowi')
 
-        config_file = open('../static/datasets/pilpres_2019_plano.json')
-        config = json.load(config_file)
-        json_file.close()
+        self.assertEqual(presidential[1]['number'], 125)
+        self.assertEqual(presidential[1]['shortName'], 'prabowo')
 
-        numbers = json_data["numbers"]
-        probabilities = []
-        for number in numbers:
-            probability_set = {"id": number["id"]}
-            number_probabilities = []
+        self.assertEqual(presidential[2]['number'], 243)
+        self.assertEqual(presidential[2]['shortName'], 'jumlah')
 
-            for digit_probability in number["extracted"]:
-                number_probabilities.append(digit_probability["probabilities"])
-            probability_set["probabilitiesForNumber"] = number_probabilities
-            probabilities.insert(0, probability_set)
-        print("probz", probabilities)
+        summary = outcomes[1][0]['numbers']
 
-        outcomes = pp.get_possible_outcomes_for_config(config, probabilities, 11)
-        most_likely = outcomes[0][0]
-        print("most likely", str(most_likely))
-        self.assertAlmostEqual(most_likely['confidence'], 0.7020011959926853)
-        self.assertEqual(most_likely['jokowi'], 186)
-        self.assertEqual(most_likely['prabowo'], 117)
-        self.assertEqual(most_likely['jumlah'], 303)
+        self.assertEqual(summary[0]['number'], 243)
+        self.assertEqual(summary[0]['shortName'], 'jumlah')
 
-    def test_get_halaman_tiga_numbers(self):
-        json_file = open('./resources/probabilities/hal3_processprobs.wsgi.json')
-        hal3_json_data = json.load(json_file)
+        self.assertEqual(summary[1]['number'], 7)
+        self.assertEqual(summary[1]['shortName'], 'tidakSah')
 
-        config_file = open('../static/datasets/hal_3_digit_config.json')
-        hal3_config = json.load(config_file)
+        self.assertEqual(summary[2]['number'], 250)
+        self.assertEqual(summary[2]['shortName'], 'jumlahSeluruh')
 
-        outcomes = pp.get_possible_outcomes_for_config(hal3_config, hal3_json_data["probabilities"], 11)
-        most_likely = outcomes[0]
-        print(str(most_likely))
-        self.assertEqual(most_likely['ADPTLaki'], 221)
-        self.assertEqual(most_likely['ADPTPerempuan'], 255)
-        self.assertEqual(most_likely['ADPTJumlah'], 476)
-        self.assertAlmostEqual(most_likely['confidence'],  0.699716392401)
+        self.assertAlmostEqual(outcomes[1][0]['confidence'], 0.7333740994255918)
 
 
 if __name__ == '__main__':
