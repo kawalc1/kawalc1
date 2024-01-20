8d9d936c148aecb4b80c5e566f5e3c071d34b1ac
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2024-01-20 17:23:48 +0800

    Test matching of forms

--- tests/extract_party_name_test.py
+++ tests/extract_party_name_test.py
@@ -5,16 +5,17 @@ from mengenali.image_classifier import detect_most_similar
 from tests import setup_django_settings
 
 
-class PartyMatchTest(unittest.TestCase):
+class ImageMatchTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
         setup_django_settings()
 
-    def assert_most_similar(self, party_file, expected_result):
-        reference_form_path = f'./resources/parties/{party_file}'
+    def assert_most_similar(self, party_file, expected_result, image_base_path: str = 'parties',
+                            feature_base_path: str = 'party_features'):
+        reference_form_path = f'./resources/{image_base_path}/{party_file}'
         image = read_image(reference_form_path)
-        party, confidence = detect_most_similar(image, 'datasets/party_features')
+        party, confidence = detect_most_similar(image, f'datasets/{feature_base_path}')
         self.assertTrue(expected_result in party, f'Expected {expected_result} in {party}')
 
     def test_detect_party_pdi(self):
@@ -25,3 +26,9 @@ class PartyMatchTest(unittest.TestCase):
 
     def test_detect_party_gerindra(self):
         self.assert_most_similar('transDPR-GERINDRA~namaPartai.jpg', 'PARTAI-GERINDRA')
+
+    def test_detect_form_second(self):
+        self.assert_most_similar('pilpres-2024-hal2.jpg', 'hal2', 'forms', 'form_features')
+
+    def test_detect_form_third(self):
+        self.assert_most_similar('pilpres-2024-hal3.jpg', 'hal3', 'forms', 'form_features')
